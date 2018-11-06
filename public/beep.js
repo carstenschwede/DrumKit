f518d900cd24f255b5fb26b98c500f8512749400
Carsten Schwede <cschwede@techfak.uni-bielefeld.de>
2018-11-06 20:01:35 +0100

    beep.js: Firefox complains about non-float value

--- public/beep.js
+++ public/beep.js
@@ -1,32 +1,32 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext
-var FREQUENCY = 440
-var INTERVAL = 250
-var RAMP_VALUE = 0.00001
-var RAMP_DURATION = 0.25
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var FREQUENCY = 440.0;
+var INTERVAL = 250.0;
+var RAMP_VALUE = 0.00001;
+var RAMP_DURATION = 0.25;
 
-var context = new window.AudioContext()
-var interval = INTERVAL
+var context = new window.AudioContext();
+var interval = INTERVAL;
 
 
 
 window.beep = function (frequency) {
-	var currentTime = context.currentTime
-	var osc = context.createOscillator()
-	var gain = context.createGain()
+	var currentTime = context.currentTime;
+	var osc = context.createOscillator();
+	var gain = context.createGain();
 
-	osc.connect(gain)
-	gain.connect(context.destination)
+	osc.connect(gain);
+	gain.connect(context.destination);
 
-	gain.gain.setValueAtTime(gain.gain.value, currentTime)
-	gain.gain.exponentialRampToValueAtTime(RAMP_VALUE, currentTime + RAMP_DURATION)
+	gain.gain.setValueAtTime(gain.gain.value, currentTime);
+	gain.gain.exponentialRampToValueAtTime(RAMP_VALUE, currentTime + RAMP_DURATION);
 
 	osc.onended = function () {
-		gain.disconnect(context.destination)
-		osc.disconnect(gain)
+		gain.disconnect(context.destination);
+		osc.disconnect(gain);
 	}
 
-	osc.type = 'sine'
-	osc.frequency.value = frequency
-	osc.start(currentTime)
-	osc.stop(currentTime + RAMP_DURATION)
+	osc.type = 'sine';
+	osc.frequency.value = frequency;
+	osc.start(currentTime);
+	osc.stop(currentTime + RAMP_DURATION);
 }
